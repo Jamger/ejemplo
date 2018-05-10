61dce663a8e2b85093b8e5da42da05d5700b27d3
Mateo <marangob1@hotmail.com>
2018-05-10 10:45:40 -0500

    Segundo commint

--- app/Http/Controllers/Browse/HomeController.php
+++ app/Http/Controllers/Browse/HomeController.php
@@ -24,7 +24,7 @@ class HomeController extends Controller
     public function indexUsers()
     {
     	$users = User::all();
-        $administrador = Role::find(1);
+        $administrador = Role::find(2);
     	//dd($users);
 
     	return view('Browse.users')
